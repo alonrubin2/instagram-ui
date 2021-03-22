1c6a2dc095d09aafb77e2ddb1241bc3ba77cccd6
Alon Rubin <alonrubin2@gmail.com>
2021-03-22 22:55:39 +0200

    will it work now?

--- src/Register/register.schema.js
+++ src/Register/register.schema.js
@@ -1,6 +1,7 @@
 import * as yup from 'yup';
-const emailValidate = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 import environment from '../environments/index';
+const emailValidate = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 
 
 export const registerSchema = yup.object().shape({
